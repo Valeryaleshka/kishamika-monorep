19d42b9379893f37c806463d6236ef4c447a46db
Valery Aleshka <valeryaleshka@gmail.com>
2025-08-25 22:52:25 -0700

    fix

--- apps/kishamika-be/src/app.controller.ts
+++ apps/kishamika-be/src/app.controller.ts
@@ -1,13 +1,13 @@
 
-import { Controller, Get, Res } from '@nestjs/common';
-import { type Response } from 'express';
-import { join } from 'path';
+import { Controller } from '@nestjs/common';
+// import { type Response } from 'express';
+// import { join } from 'path';
 
 @Controller()
 export class AppController {
 
-  @Get('*')
-  serveFrontend(@Res() res: Response) {
-    res.sendFile(join(__dirname, '..', 'public', 'index.html'));
-  }
+  // @Get('*')
+  // serveFrontend(@Res() res: Response) {
+  //   res.sendFile(join(__dirname, '..', 'public', 'index.html'));
+  // }
 }
